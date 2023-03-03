aabf3e73ca1c0ea2eddabe5fa726068991d140b6
Matt Sutherland <mwsuther@syr.edu>
2023-03-03 11:27:10 -0800

    my_ip

---
+++ my_current_ip.py
@@ -0,0 +1,26 @@
+import requests
+
+# API endpoint URL
+url = "https://ipapi.co/json/"
+
+# Make a GET request to the API endpoint
+response = requests.get(url)
+
+# Check the response status code
+if response.status_code != 200:
+    print("Error: Unable to retrieve IP information.")
+    exit()
+
+# Parse the JSON response
+data = response.json()
+
+# Print the IP information
+print('This is your IP information evailable to the WEB')
+print()
+print("IP address:", data["ip"])
+print("Country:", data["country_name"], "(" + data["country_code"] + ")")
+print("Region:", data["region"])
+print("City:", data["city"])
+print("Latitude:", data["latitude"])
+print("Longitude:", data["longitude"])
+print("Time zone:", data["timezone"])
