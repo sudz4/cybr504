0efdf82128d4dded8d43570dec98eb3fb71f258b
Matt Sutherland <mwsuther@syr.edu>
2023-03-06 12:12:13 -0800

    proton vpn add

--- is_vpn.py
+++ is_vpn.py
@@ -10,6 +10,23 @@ def is_vpn_used():
         ipaddress.IPv4Network('103.192.152.0/22'),  # ExpressVPN
         ipaddress.IPv4Network('45.65.0.0/20'),     # NordVPN
         ipaddress.IPv4Network('198.18.0.0/15'),    # Private Internet Access
+        ipaddress.IPv4Network('185.159.131.0/24'), # Proton VPN
+        ipaddress.IPv4Network('185.159.132.0/22'), # Proton VPN
+        ipaddress.IPv4Network('185.159.136.0/22'), # Proton VPN
+        ipaddress.IPv4Network('185.159.140.0/22'), # Proton VPN
+        ipaddress.IPv4Network('185.159.144.0/22'), # Proton VPN
+        ipaddress.IPv4Network('185.159.148.0/22'), # Proton VPN
+        ipaddress.IPv4Network('185.159.152.0/22'), # Proton VPN
+        ipaddress.IPv4Network('185.159.156.0/22'), #  Sweden (SE) - Proton VPN
+        ipaddress.IPv4Network('185.159.160.0/22'), # Proton VPN
+        ipaddress.IPv4Network('185.159.164.0/22'), # Proton VPN
+# 185.159.168.0/22
+# 185.159.172.0/22
+# 185.159.176.0/22
+# 185.159.180.0/22
+# 185.159.184.0/22
+# 185.159.188.0/22
+
         # Add more VPN IP address ranges as needed
     ]
     
